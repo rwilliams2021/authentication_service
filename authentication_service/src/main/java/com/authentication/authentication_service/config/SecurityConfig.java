d66c311b50ecce754adba279459a32c8f4d82531
r.williams <richyrichw93@gmail.com>
2025-06-08 13:48:32 +0200

    initial

---
+++ authentication_service/src/main/java/com/authentication/authentication_service/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.authentication.authentication_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+public class SecurityConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                                                                .requestMatchers("/home").permitAll()
+                                                                .anyRequest().authenticated())
+            .oauth2Login(AbstractAuthenticationFilterConfigurer::permitAll)
+            .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()));
+        return http.build();
+    }
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(
+                        this.clientRegistrationRepository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8081/home");
+
+        return oidcLogoutSuccessHandler;
+    }
+}
